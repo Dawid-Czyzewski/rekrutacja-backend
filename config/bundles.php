ecc900f8a8784eb8dd065570ffd5c0d17ae2abcf
dawid <dawidek12.04@wp.pl>
2024-03-30 16:17:51 +0100

    add api platform

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true]
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true]
 ];
